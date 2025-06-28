b563f486dbd3597006677b6c1880506d6b63f8cf
ZiggiZagga <75372480+ZiggiZagga@users.noreply.github.com>
2025-06-28 15:28:23 +0200

    Update DiscoveryApp.java

--- src/main/java/com/ironbucket/buzzlevane/DiscoveryApp.java
+++ src/main/java/com/ironbucket/buzzlevane/DiscoveryApp.java
@@ -8,6 +8,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class DiscoveryApp {
 	public static void main(String[] args) {
-		SpringApplication.run(DiscoveryServiceApplication.class, args);
+		SpringApplication.run(DiscoveryApp.class, args);
 	}
 }
